64a88aea24d0227605ed8bbb99c6f14c6fc7a5c5
shivamacshack <s.gaur@uq.edu.au>
2025-03-17 22:48:35 +1000

    wip

--- spring-jpa/src/main/java/com/spring_data_demo/spring_jpa/SpringJpaApplication.java
+++ spring-jpa/src/main/java/com/spring_data_demo/spring_jpa/SpringJpaApplication.java
@@ -1,13 +1,30 @@
 package com.spring_data_demo.spring_jpa;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.spring_data_demo.spring_jpa.repository.PersonJpaRepository;
+
 @SpringBootApplication
-public class SpringJpaApplication {
+public class SpringJpaApplication implements CommandLineRunner{
+
+	@Autowired
+	PersonJpaRepository personJpaRepository;
+
+	org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaApplication.class, args);
+	
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("User by id 1003-> {}", personJpaRepository.findById(1003));
+	
 	}
 
 }
